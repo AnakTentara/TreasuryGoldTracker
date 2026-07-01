ae274d116faae1027c42b1c13edb37974ff55dc9
anaktentara25@gmail.com <anaktentara25@gmail.com>
2026-07-01 15:49:43 +0700

    update server.ts

--- server.ts
+++ server.ts
@@ -16,7 +16,7 @@ async function startServer() {
   }
 
   const app = express();
-  const PORT = 3000;
+  const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 
   // Express parser
   app.use(express.json());
